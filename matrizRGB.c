dff4b30b664bcb7b7a223ce684d975d227ac6103
edurefatti <edurefatti@gmail.com>
2026-04-18 20:46:10 -0300

    Update matrizRGB.c

--- matrizRGB.c
+++ matrizRGB.c
@@ -10,7 +10,7 @@ int main()
 {
     int mat[4][4][3]= {0},r[4][4],g[4][4],b[4][4],media[4][4],l=0,somaR=0,somaB=0,somaG=0,maiorpixel=0,somapixel=0,imaior=0,jmaior=0;
     float porcen, mediaR=0, mediaG=0,mediaB=0;
-    srand(time(NULL)); //Define a seed dos numeros aleatorios para o tempo em segundos desde 1970, assim a seed nunca se repetira.
+    srand(time(NULL)); //Define a seed dos numeros aleatórios para o tempo em segundos desde 1970, assim a seed nunca se repetirá.
     printf("**** Imagem colorida (4x4):\n\n");
     for (int i = 0; i < 4; i++) // Sequencia de 3 "for" para definir a matriz 3D
     {
@@ -21,10 +21,10 @@ int main()
 
             for (int k = 0; k < 3; k++)
             {
-                mat[i][j][k]=0 + (rand() % (255 - 0 + 1)); //Formula para gerar um numero aleatorio entre 0 e 255 para os campos R, G e B
+                mat[i][j][k]=0 + (rand() % (255 - 0 + 1)); //Formula para gerar um número aleatório entre 0 e 255 para os campos R, G e B
 
             }
-//Agora o programa vai definir cada R, G e B (matrizes 4x4) com um numero aleatorio e adicionar na soma total, que vai ajudar a calcular a media de cada um mais pra frente, alem de calcular a media do pixel.
+//Agora o programa vai definir cada R, G e B (matrizes 4x4) com um numero aletório e adicionar na soma total, que vai ajudar a calcular a média de cada um mais pra frente, alem de calcular a média do pixel.
             r[i][j]= mat[i][j][0]; 
             g[i][j]= mat[i][j][1];
             b[i][j]= mat[i][j][2];
@@ -33,7 +33,7 @@ int main()
             somaB+=b[i][j];
             media[i][j] = (float)(r[i][j]+g[i][j]+b[i][j])/3;
             somapixel = r[i][j]+g[i][j]+b[i][j];
-            if (maiorpixel<somapixel) //Condicional que guarda o valor da soma de R, G e B(intensidade) e a posicao do pixel mais brilhante
+            if (maiorpixel<somapixel) //Condicional que guarda o valor da soma de R, G e B(intensidade) e a posição do pixel mais brilhante
             {
                 maiorpixel=somapixel;
                 imaior=i;
@@ -42,13 +42,13 @@ int main()
 
             printf("(R:%d, G:%d, B:%d)\t",r[i][j],g[i][j],b[i][j]); // Esse printf imprime todos os 16 pixels com seus respectivos R,G e B.
         }
-        printf("\n"); // Separa as linhas da matriz com um "enter" apos o fim de cada linha
+        printf("\n"); // Separa as linhas da matriz com um "enter" após o fim de cada linha
     }
-    // Agora, o programa calculara a media de todos os R, G e b de todos os pixel da matriz.
+    // Agora, o programa calculará a média de todos os R, G e b de todos os pixel da matriz.
     mediaR = (float)somaR/ 16;
     mediaG = (float)somaG/ 16;
     mediaB = (float)somaB/ 16;
-    //Esses printf imprimem a medias dos canais R, G e B, o pixel mais brilhante e sua posicao e a imagem(matriz) em escala cinza, a partir do loop de dois "for".
+    //Esses printf imprimem a médias dos canais R, G e B, o pixel mais brilhante e sua posicao e a imagem(matriz) em escala cinza, a partir do loop de dois "for".
     printf("\n**** Media dos canais:\n R:%.2f\n G:%.2f\n B:%.2f",mediaR, mediaG,mediaB);
     printf("\n\n**** Pixel mais brilhante: posicao (%d,%d), intensidade = %d",imaior,jmaior,maiorpixel);
     printf("\n\n**** Imagem em escala de cinza:\n");
